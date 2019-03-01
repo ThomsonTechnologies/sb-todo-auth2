b0e0f8c4d1bf456795d8592e7b2e7ab3db75010c
thomson-mathew <thomsonscode@gmail.com>
2019-02-28 23:49:58 -0600

    Complted the project
    Complted the project

---
+++ src/main/java/com/thomsoncodes/todo/SbTodoAuth2Application.java
@@ -0,0 +1,13 @@
+package com.thomsoncodes.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTodoAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTodoAuth2Application.class, args);
+	}
+
+}
